f9cd5a8ba3481a784ce8b59ca8dcaf129fc47934
soniakashyap001 <soniakashyap001@gmail.com>
2025-07-19 13:28:45 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Sonia Kashyap",
+    author_email="soniakashyap001@gmail.com",
+    packages=find_packages()
+)
